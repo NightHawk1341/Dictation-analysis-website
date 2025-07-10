7761092051375e61c5188c1e9722f3a7440a77c7
NightHawk1341 <nickhellstr0m@protonmail.com>
2025-07-10 22:28:31 +0200

    Connection to AlwaysData through ENV VARs

--- enregistrer_dictee.php
+++ enregistrer_dictee.php
@@ -2,7 +2,14 @@
 
 try {
     // 1. Connexion à la base de données avec PDO
-    $pdo = new PDO("mysql:host=localhost;dbname=duzhenko;charset=utf8", 'duzhenko', 'nikita!');
+    $host = getenv('DB_HOST');
+    $db   = getenv('DB_NAME');
+    $user = getenv('DB_USER');
+    $pass = getenv('DB_PASS');
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // 2. Récupération des données POST
--- get_dictee_details.php
+++ get_dictee_details.php
@@ -11,7 +11,6 @@ try {
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
